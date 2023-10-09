9d6110590d882dfa5d2de463163e5dbe9faabfee
chendeli <419287484@qq.com>
2023-10-09 23:54:28 +0800

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2023-10-09 23:48:51
+ * @LastEditTime: 2023-10-09 23:49:05
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \java\.vscode\public class HelloWorld {.java
+ * 
+ */
+public class HelloWorld {
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    System.out.println("欢迎");
+  }
+}
