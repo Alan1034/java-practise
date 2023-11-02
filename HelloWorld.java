1d6abb35f50aca83f10974977f684728f7c58f99
AlanChen <419287484@qq.com>
2023-11-02 23:53:00 +0800

    HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,20 @@
 /*
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2023-10-09 23:48:51
- * @LastEditTime: 2023-10-09 23:49:05
+ * @LastEditTime: 2023-11-02 23:52:50
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: 
- * @FilePath: \java\.vscode\public class HelloWorld {.java
+ * @FilePath: \java\HelloWorld.java
  * 
  */
 public class HelloWorld {
+  // public：访问修饰符
+  // static：关键字
+  // void：返回类型
+  // main：方法名
+  // String：String类
+  // args：字符串数组
   public static void main(String[] args){
     System.out.println("Hello World");
     System.out.println("欢迎");
